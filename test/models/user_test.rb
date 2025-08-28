e13b92046934e3a56c70691c8ab9a984dfb321d9
jk-me <36418258+jk-me@users.noreply.github.com>
2025-08-28 00:20:59 -0400

    c add test using users.rml fixture

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user_one = users(:one)
+    @user_two = users(:two)
+  end
+
+  test "should validate user one email address" do
+    assert_equal "one@example.com", @user_one.email_address
+  end
+
+  test "should validate user two email address" do
+    assert_equal "two@example.com", @user_two.email_address
+  end
+
+  test "should authenticate user with correct password" do
+    assert @user_one.authenticate("password")
+  end
+
+  test "should not authenticate user with incorrect password" do
+    refute @user_one.authenticate("wrongpassword")
+  end
 end

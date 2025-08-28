f796daa3e18768417481c94cb14f958803176bf7
jk-me <36418258+jk-me@users.noreply.github.com>
2025-08-28 12:52:54 -0400

    clean up login flow and forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resource :session
-  resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -14,6 +12,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "welcome#index"
 
+  resource :session, only: %i[ create destroy ]
+  resources :passwords, param: :token
+  get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   get "/signup", to: "users#new"
 end
